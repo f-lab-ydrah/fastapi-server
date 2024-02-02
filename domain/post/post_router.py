2ffc5da9baa9c950747c5a711cf30e10c2bd8b55
8bitHermitcrab <8bithermitcrab@gmail.com>
2024-02-02 22:13:23 +0900

    Feature/api (#10)
    * endpoint, 함수명 변경
    
    * 피드백 반영 수정 진행중
    
    * 피드백 반영 코드 수정중

--- domain/post/post_router.py
+++ domain/post/post_router.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import datetime
 
 from domain.post import post_schema
 
@@ -9,12 +10,13 @@ router = APIRouter(
 
 store_data = {}
 
+store_data = {}
+
 @router.post("/post")
 def create_post(data: post_schema.Item):
     """
     게시글 생성
     """
-    # DB에 데이터 추가
     return data
 
 @router.get("/posts")
@@ -22,7 +24,6 @@ def get_posts():
     """
     게시글 목록 조회
     """
-    # DB에서 데이터 가져오기
     post_id = 1
     author = 'author1'
     title = 'title1'
@@ -33,6 +34,7 @@ def get_posts():
             "data" : [{"post_id": post_id, "author": author, "title": title, "content": content, "created_at": created_at}]}
 
 @router.get("/posts/{post_id}")
+@router.get("/posts/{post_id}")
 def get_post(post_id: int):
     """
     게시글 조회
@@ -41,7 +43,7 @@ def get_post(post_id: int):
     author = 'author1'
     title = 'title1'
     content = 'content1'
-    created_at = '2024-01-01'
+    created_at = datetime.datetime
     return {"code" : "200", 
             "message" : "성공", 
             "data" : {"post_id": post_id, 
