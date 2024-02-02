3747a64f59a4f0821d3347c525cbc276fcb2bc91
kij <8bithermitcrab@gmail.com>
2024-02-02 22:09:13 +0900

    피드백 반영 코드 수정중

--- domain/post/post_router.py
+++ domain/post/post_router.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import datetime
 
 from domain.post import post_schema
 
@@ -14,7 +15,6 @@ def create_post(data: post_schema.Item):
     """
     게시글 생성
     """
-    # DB에 데이터 추가
     return data
 
 @router.get("/posts")
@@ -22,7 +22,6 @@ def get_posts():
     """
     게시글 목록 조회
     """
-    # DB에서 데이터 가져오기
     post_id = 1
     author = 'author1'
     title = 'title1'
@@ -41,7 +40,7 @@ def get_post(post_id: int):
     author = 'author1'
     title = 'title1'
     content = 'content1'
-    created_at = '2024-01-01'
+    created_at = datetime.datetime
     return {"code" : "200", 
             "message" : "성공", 
             "data" : {"post_id": post_id, 
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse, JSONResponse
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException
 
 from domain.post import post_router
 
@@ -8,5 +10,19 @@ app = FastAPI()
 @app.get("/", response_class=RedirectResponse)
 def index():
     return "/docs"
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request: Request, exc):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"message" : f"{exc.name}"}
+    )
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc):
+    return JSONResponse(
+        status_code=404,
+        content={"message" : f"{exc.name}"}
+    )
 
 app.include_router(post_router.router)
