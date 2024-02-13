7a1814f9e91d7890a78e22e9bd5ff3b7f71a20c8
kij <8bithermitcrab@gmail.com>
2024-02-13 20:16:10 +0900

    posts.db, posts 테이블 생성

--- service/database.py
+++ service/database.py
@@ -1,14 +1,22 @@
-# import sqlite3
+import sqlite3
 
-# conn = sqlite3.connect(".db")
+conn = sqlite3.connect("posts.db")
 
-# cur = conn.cursor()
+cur = conn.cursor()
 
-# cur.execute("SELECT * FROM ")
+cur.execute("""
+            CREATE TABLE posts (
+                post_id integer primary key autoincrement,
+                author text,
+                title text,
+                content text,
+                created_at sysdate
+                )
+            """)
 
-# rows = cur.fetchall()
+rows = cur.fetchall()
 
-# for row in rows:
-#     print(row)
+for row in rows:
+    print(row)
     
-# conn.close()
+conn.close()
